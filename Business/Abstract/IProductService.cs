1da1a0343c0c9c3a36f141805d6a2f2dc850bee0
BerkanKmrc1 <70950273+BerkanKmrc1@users.noreply.github.com>
2021-09-30 18:56:27 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal max, decimal min);
+        IDataResult<Product> GetById(int id);
+        IDataResult <List<ProductDetailDto>> GetProductDetails();
+        IResult Add(Product product);
+        
+
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var categoy in categoryManager.GetAll())
+            {
+                Console.WriteLine(categoy.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            var result = productManager.GetProductDetails();
+            if (result.Success==true)
+            {
+                foreach (var item in result.Data)
+                {
+                    Console.WriteLine(item.ProductName + "/" + item.CategoryName);
+                }
+            }
+            else
+            {
+                Console.Write(result.Message);
+            }
+                
+        }
+    }
+}
